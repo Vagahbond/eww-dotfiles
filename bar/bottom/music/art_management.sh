26acfc79aefc5d3a6a950af76ba2070a421d07cf
Yoni FIRROLONI <firroloni.yoni@gmail.com>
2023-06-07 04:28:36 +0200

    add spotify widget

---
+++ bar/bottom/music/art_management.sh
@@ -0,0 +1,9 @@
+CURRENT_POS=$(playerctl -p spotify position)
+
+if [ ! -f /tmp/cover_art.png ] || [ ${CURRENT_POS%.*} -lt 6 ]; then
+    curl $(playerctl -p spotify metadata mpris:artUrl) -o /tmp/cover_art.png
+
+    echo "Updating the cover art"
+fi
+
+echo "/tmp/cover_art.png"
