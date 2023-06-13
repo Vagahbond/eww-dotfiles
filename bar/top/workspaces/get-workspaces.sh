07aff4432417a40c64d96ccd2a10663e92cc21bc
Yoni FIRROLONI <firroloni.yoni@gmail.com>
2023-06-13 23:57:43 +0200

    logos and screen colors

--- bar/top/workspaces/get-workspaces.sh
+++ bar/top/workspaces/get-workspaces.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
 
 spaces (){
-	WORKSPACE_WINDOWS=$(hyprctl workspaces -j | jq 'map({key: .id | tostring, value: .windows}) | from_entries')
-	seq 1 10 | jq --argjson windows "${WORKSPACE_WINDOWS}" --slurp -Mc 'map(tostring) | map({id: ., windows: ($windows[.]//0)})'
+	WORKSPACE_WINDOWS=$(hyprctl workspaces -j | jq 'map({key: .id | tostring, value: {lastwindow: .lastwindowtitle, monitor: .monitor, windows: .windows}}) | from_entries')
+	# seq 1 10 | jq --argjson windows "${WORKSPACE_WINDOWS}" --slurp -Mc 'map(tostring) | map({id: ., windows: ($windows[.]//0)})'
+    # WORKSPACE_WINDOWS=$(hyprctl workspaces -j | jq -Mc '[.[] | {id: .id | tostring, lastwindow: .lastwindowtitle, monitor: .monitor, windows: .windows}]')
+    seq 1 10 | jq --argjson values "${WORKSPACE_WINDOWS}" --slurp -Mc 'map(tostring) | map({id: ., windows: ($values[.].windows//0), lastwindow: $values[.].lastwindow, monitor: $values[.].monitor})' \
+        | sed -E 's/lastwindow\":\"[^{]*Visual Studio Code[^}]*\",/lastwindow":"󰨞",/g' \
+        | sed -E 's/lastwindow\":\"[^{]*(Neovim|vim)[^}]*\",/lastwindow":"",/g' \
+        | sed -E 's/lastwindow\":\"[^{]*WebCord -[^}]*\",/lastwindow":"󰙯",/g' \
+        | sed -E 's/lastwindow\":\"[^{]*WhatsApp[^}]*\",/lastwindow":"",/g' \
+        | sed -E 's/lastwindow\":\"[^{]*Spotify[^}]*\",/lastwindow":"",/g' \
+        | sed -E 's/lastwindow\":\"[^{]*(bash|zsh|vagahbond@framework)[^}]*\",/lastwindow":"",/g' \
+        | sed -E 's/lastwindow\":\"[^{]* Mozilla Firefox\",/lastwindow":"󰈹",/g' \
+        | sed -E 's/lastwindow\":\"{0,1}[^{}"]{2,}\"{0,1},/lastwindow":false,/g' 
+
+
 }
 
 spaces
