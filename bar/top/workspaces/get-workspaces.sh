9110736021cf53041b1d5ab4145a387f0d31095c
Yoni FIRROLONI <pro@yoni-firroloni.fr>
2023-10-10 16:20:07 +0200

    fix: window titles on workspaces widget

--- bar/top/workspaces/get-workspaces.sh
+++ bar/top/workspaces/get-workspaces.sh
@@ -2,16 +2,19 @@
 
 spaces (){
 	WORKSPACE_WINDOWS=$(hyprctl workspaces -j | jq 'map({key: .id | tostring, value: {lastwindow: .lastwindowtitle, monitor: .monitor, windows: .windows}}) | from_entries')
-	# seq 1 10 | jq --argjson windows "${WORKSPACE_WINDOWS}" --slurp -Mc 'map(tostring) | map({id: ., windows: ($windows[.]//0)})'
-    # WORKSPACE_WINDOWS=$(hyprctl workspaces -j | jq -Mc '[.[] | {id: .id | tostring, lastwindow: .lastwindowtitle, monitor: .monitor, windows: .windows}]')
+
+
+    # replace every occurence of '\"' with ''' in WORKSPACE_WINDOWS
+    WORKSPACE_WINDOWS=$(echo $WORKSPACE_WINDOWS | sed -E "s/\\\\\"/\\'/g")
+
     seq 1 10 | jq --argjson values "${WORKSPACE_WINDOWS}" --slurp -Mc 'map(tostring) | map({id: ., windows: ($values[.].windows//0), lastwindow: $values[.].lastwindow, monitor: $values[.].monitor})' \
-        | sed -E 's/lastwindow\":\"[^{]*Visual Studio Code[^}]*\",/lastwindow":"󰨞",/g' \
-        | sed -E 's/lastwindow\":\"[^{]*(Neovim|vim)[^}]*\",/lastwindow":"",/g' \
-        | sed -E 's/lastwindow\":\"[^{]*WebCord -[^}]*\",/lastwindow":"󰙯",/g' \
-        | sed -E 's/lastwindow\":\"[^{]*WhatsApp[^}]*\",/lastwindow":"",/g' \
-        | sed -E 's/lastwindow\":\"[^{]*Spotify[^}]*\",/lastwindow":"",/g' \
-        | sed -E 's/lastwindow\":\"[^{]*(bash|zsh|vagahbond@framework)[^}]*\",/lastwindow":"",/g' \
-        | sed -E 's/lastwindow\":\"[^{]*Mozilla Firefox\",/lastwindow":"󰈹",/g' \
+        | sed -E 's/lastwindow\":\"[^{"]*Mozilla Firefox\",/lastwindow":"󰈹",/g' \
+        | sed -E 's/lastwindow\":\"[^{"]*Visual Studio Code[^}"]*\",/lastwindow":"󰨞",/g' \
+        | sed -E 's/lastwindow\":\"[^{"]*(Neovim|vim)[^}"]*\",/lastwindow":"",/g' \
+        | sed -E 's/lastwindow\":\"[^{"]*Discord[^}]*\",/lastwindow":"󰙯",/g' \
+        | sed -E 's/lastwindow\":\"[^{"]*WhatsApp[^}"]*\",/lastwindow":"",/g' \
+        | sed -E 's/lastwindow\":\"[^{"]*Spotify[^}"]*\",/lastwindow":"",/g' \
+        | sed -E 's/lastwindow\":\"[^{"]*foot[^"}]*\",/lastwindow":"",/g' \
         | sed -E 's/lastwindow\":\"{0,1}[^{}"]{2,}\"{0,1},/lastwindow":false,/g' \
         | sed -E 's/lastwindow\":\"\",/lastwindow":false,/g' 
 
